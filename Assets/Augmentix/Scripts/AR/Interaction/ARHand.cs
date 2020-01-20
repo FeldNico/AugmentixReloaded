bcec8cd41d436fa96f95686b1499f175b1835729
FeldNico <feldn@uni-trier.de>
2020-01-20 11:50:13 +0100

    added oculus integration

---
+++ Assets/Augmentix/Scripts/AR/Interaction/ARHand.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ARHand : MonoBehaviour
+{
+    public bool IsRight;
+    public GameObject Thumb;
+    public GameObject IndexFinger;
+    [HideInInspector] public float PinchStrength;
+    [HideInInspector] public bool IsDetected = false;
+    [HideInInspector] public bool IsPointing = false;
+    [HideInInspector] public Vector3 PointingDirection;
+    public UnityAction OnDetect;
+    public UnityAction OnLost;
+    public UnityAction OnPointStart;
+    public UnityAction OnPointEnd;
+    public UnityAction OnPinchStart;
+    public UnityAction OnPinchEnd;
+    
+    public Interactable GrabbedInteractable { private set; get; }
+
+    #region DEBUG
+
+    private LineRenderer _lineRenderer;
+
+    #endregion
+    
+    public ARHand()
+    {
+        OnDetect += () =>
+        {
+            gameObject.SetActive(true);
+        };
+        OnLost += () =>
+        {
+            gameObject.SetActive(false);
+        };
+        OnPinchEnd += () => { Debug.Log("OnPinchEnd"); };
+        OnPinchStart += () =>
+        {
+            Debug.Log("OnPinchStart");
+            if (GrabbedInteractable == null)
+            {
+                
+            }
+        };
+        OnPointEnd += () => { Debug.Log("OnPointEnd"); };
+        OnPointStart += () => { Debug.Log("OnPointStart"); };
+    }
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private bool _wasPinching = false;
+    public void Update()
+    {
+        if (IsPinching() ^ _wasPinching) 
+        {
+            if (_wasPinching)
+            {
+                _wasPinching = false;
+                OnPinchEnd?.Invoke();
+            }
+            else
+            {
+                _wasPinching = true;
+                OnPinchStart?.Invoke();
+            }
+        }
+
+        if (IsPointing)
+        {
+            if (!_lineRenderer.enabled)
+                _lineRenderer.enabled = true;
+            _lineRenderer.SetPosition(0, IndexFinger.transform.localPosition);
+            _lineRenderer.SetPosition(1, IndexFinger.transform.localPosition + PointingDirection * 5);
+        }
+        else
+        {
+            if (_lineRenderer.enabled)
+                _lineRenderer.enabled = false;
+            _lineRenderer.SetPosition(0, Vector3.zero);
+            _lineRenderer.SetPosition(1, Vector3.zero);
+        }
+    }
+
+
+    public bool IsPinching()
+    {
+        return PinchStrength > 0.8F;
+    }
+    
+    public Vector3 GetPinchPosition() {
+        return (2 * Thumb.transform.position + IndexFinger.transform.position) * 0.333333F;
+    }
+}
---
+++ Assets/Augmentix/Scripts/AR/Interaction/Interactable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Collider _collider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+        if (_collider == null)
+            Debug.LogError("Interactable must have an collider!");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
