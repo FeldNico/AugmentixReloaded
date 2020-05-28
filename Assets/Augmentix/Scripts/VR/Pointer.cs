17284bb774d488988187c22aba42258db81b7b94
FeldNico <feldn@uni-trier.de>
2020-05-28 12:38:00 +0200

    fixed VR scene + lightning

---
+++ Assets/Augmentix/Scripts/VR/Pointer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pointer : MonoBehaviour
+{
+    
+    public GameObject Dot;
+    public PointerTarget CurrentPointerTarget { private set; get; }
+
+    private LineRenderer lineRenderer;
+    private float MaxLength = 5f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        
+        /*
+        OnClick.AddOnStateDownListener((action, source) =>
+        {
+            if (CurrentPointerTarget == null)
+                return;
+
+            var pos = CurrentPointerTarget.transform.InverseTransformPoint(Dot.transform.position);
+            
+            CurrentPointerTarget.OnPress?.Invoke(new Vector2(pos.x,pos.z));
+        },SteamVR_Input_Sources.RightHand);
+        
+        OnClick.AddOnStateUpListener((action, source) =>
+        {
+            if (CurrentPointerTarget == null)
+                return;
+
+            var pos = CurrentPointerTarget.transform.InverseTransformPoint(Dot.transform.position);
+            
+            CurrentPointerTarget.OnRelease?.Invoke(new Vector2(pos.x,pos.z));
+        },SteamVR_Input_Sources.RightHand);
+        */
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        RaycastHit hit;
+        var ray = new Ray(transform.position,transform.forward);
+        Physics.Raycast(ray, out hit, MaxLength);
+
+        if (hit.collider != null)
+        {
+            var pointerTarget = hit.transform.GetComponent<PointerTarget>();
+
+            if (pointerTarget == null)
+            {
+                Dot.SetActive(false);
+                lineRenderer.enabled = false;
+                return;
+            }
+            Dot.SetActive(true);
+            lineRenderer.enabled = true;
+            Dot.transform.position = hit.point;
+            lineRenderer.SetPosition(0,transform.position);
+            lineRenderer.SetPosition(1,Dot.transform.position);
+
+            if (pointerTarget != CurrentPointerTarget)
+            {
+                if (CurrentPointerTarget != null)
+                {
+                    CurrentPointerTarget.OnHoverEnd?.Invoke();
+                }
+                pointerTarget.OnHoverStart?.Invoke();
+                CurrentPointerTarget = pointerTarget;
+            }
+        }
+        else
+        {
+            Dot.SetActive(false);
+            lineRenderer.enabled = false;
+
+            if (CurrentPointerTarget != null)
+            {
+                CurrentPointerTarget.OnHoverEnd?.Invoke();
+                //Dot.gameObject.SetActive(false);
+            }
+            CurrentPointerTarget = null;
+        }
+    }
+}
---
+++ Assets/Augmentix/Scripts/VR/PointerTarget.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Augmentix.Scripts;
+using Augmentix.Scripts.VR;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class PointerTarget : MonoBehaviour
+{
+    public UnityAction OnHoverStart;
+    public UnityAction OnHoverEnd;
+    public UnityAction<Vector2> OnPress;
+    public UnityAction<Vector2> OnRelease;
+
+    public Vector3 TeleportTarget;
+
+    private Button _button;
+    public void Awake()
+    {
+        _button = GetComponent<Button>();
+        
+        /*
+        OnRelease += pos =>
+        {
+            if (!TeleportTarget.Equals(Vector3.zero))
+            {
+                var fadeDuraction = ((StandaloneTargetManager) TargetManager.Instance).TeleportFadeDuration;
+                
+                SteamVR_Fade.Start( Color.clear, 0 );
+                SteamVR_Fade.Start( Color.black, fadeDuraction );
+                FindObjectOfType<Player>().transform.position = TeleportTarget;
+
+                StartCoroutine(WaitForFade());
+                
+                IEnumerator WaitForFade()
+                {
+                    yield return new WaitForSeconds(fadeDuraction);
+                    SteamVR_Fade.Start( Color.clear, fadeDuraction );
+                }
+
+            }
+        };
+
+        if (_button != null)
+        {
+            OnRelease += pos =>
+            {
+                var colors = _button.colors;
+                var normal = colors.normalColor;
+                colors.normalColor = colors.selectedColor;
+                colors.selectedColor = normal;
+                _button.colors = colors;
+            };
+            
+            OnPress += pos =>
+            {
+                var colors = _button.colors;
+                var normal = colors.normalColor;
+                colors.normalColor = colors.selectedColor;
+                colors.selectedColor = normal;
+                _button.colors = colors;
+            };
+
+            OnHoverStart += () =>
+            {
+                var colors = _button.colors;
+                var normal = colors.normalColor;
+                colors.normalColor = colors.highlightedColor;
+                colors.highlightedColor = normal;
+                _button.colors = colors;
+            };
+        
+            OnHoverEnd += () =>
+            {
+                var colors = _button.colors;
+                var normal = colors.normalColor;
+                colors.normalColor = colors.highlightedColor;
+                colors.highlightedColor = normal;
+                _button.colors = colors;
+            };
+        }
+        */
+        
+    }
+}
